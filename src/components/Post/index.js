a3b220ba3895a5f35ac49a750bee6b31729a2149
Mickael Rocha <51544205+mickaelxd@users.noreply.github.com>
2020-03-07 09:37:39 -0300

    feat: Post Section and CSS

---
+++ src/components/Post/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import './styles.css';
+
+function PostHeader({author, date}) {
+  return (
+    <div className="post-header">
+       <img className="avatar" src={author.avatar} alt={author.name} />
+       <div className="details">
+        <span>{author.name}</span>
+        <span>{date}</span>
+      </div>
+    </div>
+    );
+}
+
+function PostComments({ comments }) {
+  return (
+    <div className="post-comments">
+      <div className="divider" />
+      {comments.map(comment => (
+        <div key={comment.id} className="comment">
+          <img className="avatar" src={comment.author.avatar} alt={comment.author.name} />
+          <p>
+            <span>{comment.author.name}</span>
+            {comment.content}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+
+function PostItem({ author, date, content, comments }) {
+  return(
+    <div className="post">
+      <PostHeader author={author} date={date} />
+      <p className="content">{content}</p>
+      <PostComments comments={comments} />
+    </div>
+  )
+}
+
+export default PostItem;

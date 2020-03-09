b599c8f94455c95549f686004286330998b13d08
Mickael Rocha <51544205+mickaelxd@users.noreply.github.com>
2020-03-09 08:43:39 -0300

    feat: change all css to one file
    Remove all other .css files and put everything on src/styles.css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,19 +3,17 @@ import React, {Component} from 'react';
 import Logo from '../../assets/logo.png';
 import Brasao from '../../assets/logo_brasao.png';
 
-import './styles.css';
-
 export default class Header extends Component{
   render(){
     return(
       <header className="header">
-        <div className="content">
+        <nav className="content">
           <img src={Logo} alt="Logo"/>
           <div className="profile">
             <strong>Profile</strong>
             <img src={Brasao} alt="Profile" />
           </div>
-        </div>
+        </nav>
       </header>
     )
   }
